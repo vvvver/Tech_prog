b9eae4c30741da15d44554770437c91422d1e4c5
aakovaall <159827010+aakovaall@users.noreply.github.com>
2024-05-29 17:01:16 +0300

    comment navigation_page.h

--- client_interface/navigation_page.h
+++ client_interface/navigation_page.h
@@ -18,22 +18,31 @@ public:
     QString userLog;
     QString graph;
 private slots:
+    // \brief слот реагирующий на открытие первого задания
     void on_task1_Button_clicked();
 
+    // \brief слот реагирующий на выход из первого задания
     void on_exit1_Button_clicked();
 
+    // \brief слот реагирующий на открытие второго задания
     void on_task2_Button_clicked();
 
+    // \brief слот реагирующий на кнопку выхода к меню
     void on_exit_home_clicked();
 
+    // \brief слот реагирующий на кнопку назад
     void on_exitButton_clicked();
 
+    // \brief слот открывающий статистику
     void on_stat_Button_clicked();
 
+    // \brief слот реагирующий на кнопку назад
     void on_exit_Button_clicked();
 
+    // \brief слот реагирующий на открытие первого задания
     void on_submit_task1_button_clicked();
 
+    // \brief слот реагирующий на открытие второго задания
     void on_submit_task2_Button_clicked();
 
 private:
